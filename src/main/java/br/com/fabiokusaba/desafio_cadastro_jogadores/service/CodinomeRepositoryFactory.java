29d41abd785453c10066477a755dc1277f5ff23a
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-09 20:40:24 -0300

    implementação do padrão Factory para retornar um repositório específico com base no grupoCodinome

---
+++ src/main/java/br/com/fabiokusaba/desafio_cadastro_jogadores/service/CodinomeRepositoryFactory.java
@@ -0,0 +1,33 @@
+package br.com.fabiokusaba.desafio_cadastro_jogadores.service;
+
+import org.springframework.stereotype.Component;
+
+import br.com.fabiokusaba.desafio_cadastro_jogadores.model.GrupoCodinome;
+import br.com.fabiokusaba.desafio_cadastro_jogadores.repository.CodinomeRepository;
+import br.com.fabiokusaba.desafio_cadastro_jogadores.repository.LigaDaJusticaRepository;
+import br.com.fabiokusaba.desafio_cadastro_jogadores.repository.VingadoresRepository;
+
+@Component
+public class CodinomeRepositoryFactory {
+    
+    private final LigaDaJusticaRepository ligaDaJusticaRepository;
+    private final VingadoresRepository vingadoresRepository;
+
+    
+
+    public CodinomeRepositoryFactory(LigaDaJusticaRepository ligaDaJusticaRepository,
+            VingadoresRepository vingadoresRepository) {
+        this.ligaDaJusticaRepository = ligaDaJusticaRepository;
+        this.vingadoresRepository = vingadoresRepository;
+    }
+
+
+
+    public CodinomeRepository create(GrupoCodinome grupoCodinome) {
+        return switch (grupoCodinome) {
+            case LIGA_DA_JUSTICA -> ligaDaJusticaRepository;
+            case VINGADORES -> vingadoresRepository;
+        };
+    }
+
+}
