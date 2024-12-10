d76a3c19d7dc8a2cf2158b489380f2b07005cba0
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-09 22:20:25 -0300

    adicionado model jogador

--- src/main/java/br/com/fabiokusaba/desafio_cadastro_jogadores/web/CadastroJogadorController.java
+++ src/main/java/br/com/fabiokusaba/desafio_cadastro_jogadores/web/CadastroJogadorController.java
@@ -23,6 +23,7 @@ public class CadastroJogadorController {
 
     @GetMapping
     public String paginaCadastroJogador(Model model) {
+        model.addAttribute("jogador", new Jogador(null, null, null, null, null));
         model.addAttribute("gruposCodinomes", GrupoCodinome.values());
         return "cadastro_jogador";
     }
