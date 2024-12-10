e651801b618a9f5053c3afc182431814dbbae09e
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-09 21:18:03 -0300

    criação do DTO responsável pelos dados da requisição

---
+++ src/main/java/br/com/fabiokusaba/desafio_cadastro_jogadores/web/LigaDaJusticaDTO.java
@@ -0,0 +1,28 @@
+package br.com.fabiokusaba.desafio_cadastro_jogadores.web;
+
+import java.util.List;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "liga_da_justica")
+public record LigaDaJusticaDTO(
+    @JacksonXmlProperty(localName = "codinomes")
+    CodinomesDTO codinomes
+) implements CodinomeDTO {
+
+    @Override
+    public List<String> getCodinomes() {
+        return codinomes.codinomes();
+    }
+
+}
+
+record CodinomesDTO(
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "codinome")
+    List<String> codinomes
+) {
+
+}
